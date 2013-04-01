9f65c41d1f780f2dd9190ea59ef6737debefeedd
DBasarab <dbasarab617@gmail.com>
2013-03-31 23:46:39 -0400

    Next step in the tutorial.  Part 9 Complete.

--- js/homeView.js
+++ js/homeView.js
@@ -1,4 +1,4 @@
-var HomeView = function(store) {
+var HomeView = function (store) {
 
     this.initialize = function () {
         // Define a div wrapper for the view.  The div wrapper is used to attach events
@@ -11,8 +11,8 @@ var HomeView = function(store) {
         return this;
     };
 
-    this.findByName = function() {
-        store.findByName($('.search-key').val(), function(employees) {
+    this.findByName = function () {
+        store.findByName($('.search-key').val(), function (employees) {
             $('.employee-list').html(HomeView.liTemplate(employees));
             if (self.iscroll) {
                 console.log('Refresh iScroll');
--- js/main.js
+++ js/main.js
@@ -1,42 +1,58 @@
 var app = {
 
 
-    showAlert: function(message, title) {
-        if(navigator.notification) {
+    showAlert: function (message, title) {
+        if (navigator.notification) {
             navigator.notification.alert(message, null, title, 'OK');
         } else {
             alert(title ? (title + ": " + message) : message);
         }
     },
 
-    registerEvents: function() {
-      var self = this;
+    route: function () {
+        var hash = window.location.hash;
+        if (!hash) {
+            $('body').html(new HomeView(this.store).render().el);
+            return;
+        }
+        var match = hash.match(app.detailsURL);
+        if (match) {
+            this.store.findById(Number(match[1]), function (employee) {
+                $('body').html(new EmployeeView(employee).render().el);
+            })
+        }
+    },
+
+    registerEvents: function () {
+        var self = this;
 
         // Check of browser supports touch events . . .
         if (document.documentElement.hasOwnProperty('ontouchstart')) {
             // If yes: register touch event listen to change the "selected" state of the item
-            $('body').on('touchstart', 'a', function(event){
+            $('body').on('touchstart', 'a', function (event) {
                 $(event.target).addClass('tappable-active');
             });
-            $('body').on('touchend', 'a', function(event){
+            $('body').on('touchend', 'a', function (event) {
                 $(event.target).removeClass('tappable-active');
             });
         } else {
             // if not: register mouse events instead
-            $('body').on('mousedown', 'a', function(event){
+            $('body').on('mousedown', 'a', function (event) {
                 $(event.target).addClass('tappable-active');
             });
-            $('body').on('mouseup', 'a', function(event){
+            $('body').on('mouseup', 'a', function (event) {
                 $(event.target).removeClass('tappable-active');
             });
         }
+
+        $(window).on('hashchange', $.proxy(this.route, this));
     },
 
     initialize: function () {
         var self = this;
-
-        this.store = new MemoryStore(function(){
-            $('body').html(new HomeView(self.store).render().el);
+        this.detailsURL = /^#employees\/(\d{1,})/;
+        this.store = new MemoryStore(function () {
+            self.route();
         });
 
         self.registerEvents();
