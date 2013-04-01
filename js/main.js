d82139687e1cc74a92914f3dc2bfc4dc44c3926e
DBasarab <dbasarab617@gmail.com>
2013-03-31 23:04:30 -0400

    Next step in the tutorial.  Part 8 Complete.

--- js/main.js
+++ js/main.js
@@ -9,12 +9,37 @@ var app = {
         }
     },
 
-        initialize: function () {
+    registerEvents: function() {
+      var self = this;
+
+        // Check of browser supports touch events . . .
+        if (document.documentElement.hasOwnProperty('ontouchstart')) {
+            // If yes: register touch event listen to change the "selected" state of the item
+            $('body').on('touchstart', 'a', function(event){
+                $(event.target).addClass('tappable-active');
+            });
+            $('body').on('touchend', 'a', function(event){
+                $(event.target).removeClass('tappable-active');
+            });
+        } else {
+            // if not: register mouse events instead
+            $('body').on('mousedown', 'a', function(event){
+                $(event.target).addClass('tappable-active');
+            });
+            $('body').on('mouseup', 'a', function(event){
+                $(event.target).removeClass('tappable-active');
+            });
+        }
+    },
+
+    initialize: function () {
         var self = this;
 
         this.store = new MemoryStore(function(){
             $('body').html(new HomeView(self.store).render().el);
         });
+
+        self.registerEvents();
     }
 };
 
