715e9dc22e7cdcf5dfe190704959c3d252adb41b
David Basarab <dbasarab617@gmail.com>
2013-04-01 11:30:37 -0400

    Fixing

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ var app = {
 
 
     showAlert: function (message, title) {
+        console.log(message);
         if (navigator.notification) {
             navigator.notification.alert(message, null, title, 'OK');
         } else {
